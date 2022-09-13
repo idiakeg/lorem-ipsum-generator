c12f56d9c0b9a17a8a5012076f1d2030db490194
Godspower Idiakose <idiakeg@gmail.com>
2022-09-13 20:54:36 +0100

    created the lorem ipsum generator and implemented the number generator functionality.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import data from "./data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [number, setNumber] = useState(0);
+	const [text, setText] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let goo = data.slice(0, number);
+		setText(goo);
+		// console.log(text);
+	};
+
+	const handleChange = (value) => {
+		if (value <= 8 && value >= 0) {
+			setNumber(value);
+		}
+	};
+
+	return (
+		<div className="app">
+			<header>
+				<h1>tired of boring lorem ipsum?</h1>
+			</header>
+			<section>
+				<form onSubmit={handleSubmit}>
+					<label htmlFor="number">
+						paragraph:
+						<input
+							type="number"
+							value={number}
+							onChange={(e) => handleChange(e.target.value)}
+						/>
+					</label>
+					<button type="submit">generate</button>
+				</form>
+				{text.map((item, index) => (
+					<p key={index}>{item}</p>
+				))}
+			</section>
+		</div>
+	);
 }
 
 export default App;
